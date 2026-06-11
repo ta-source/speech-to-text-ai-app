cb60e858e5ae2ee3975bc7bf8b8f51b36a4d6713
Tania Roy <tania15roy@gmail.com>
2026-06-11 13:19:34 +0530

    Update requirements for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-import generate_email
 from flask import (
     Flask,
     jsonify,
@@ -381,7 +380,5 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=5000,
-        debug=True,
-        allow_unsafe_werkzeug=True
+        port=int(os.environ.get("PORT", 5000))
     )
