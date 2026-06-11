b7c6da13b1967afe69babda8346802777b67ee5b
Tania Roy <tania15roy@gmail.com>
2026-06-11 14:14:48 +0530

    fix render port

--- backend/app.py
+++ backend/app.py
@@ -377,8 +377,10 @@ def translate():
     })
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=port
     )
